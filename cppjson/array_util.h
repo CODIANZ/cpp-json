646c2ac9436a7b1d471131369b0789a12f7bc04d
terukazu inoue <terukazu.inoue@codianz.com>
2021-11-25 20:28:50 +0900

    object と array のユーティリティクラスをリファクタリング

--- cppjson/array_util.h
+++ cppjson/array_util.h
@@ -11,25 +11,23 @@ class array_util {
 
 public:
   template<typename ITER> static json to_json(ITER begin, ITER end) {
-    json j = json::array_type();
-    auto& arr = j.get<json::array_type>();
-    for(ITER it = begin; it != end; it++){
-      arr.push_back(*it);
-    }
-    return j;
+    return create([&](json::array_type& arr){
+      arr.insert(arr.begin(), begin, end);
+    });
   }
 
   template<typename T> static json to_json(const T& src){
     return to_json(std::cbegin(src), std::cend(src));
   }
 
-  static json create(std::function<void(json::array_type&)> f = std::function<void(json::array_type&)>()) {
-    json j = json::array_type();
-    auto& arr = j.get<json::array_type>();
-    if(f){
-      f(arr);
-    }
+  static json create(std::function<void(json::array_type&)> f) {
+    json j = create();
+    f(j.get<json::array_type>());
     return j;
+  }
+
+  static json create() {
+    return json::array_type();
   }
 
   static json& edit(json& j, std::function<void(json::array_type&)> f){
--- cppjson/object_util.h
+++ cppjson/object_util.h
@@ -11,25 +11,23 @@ class object_util {
 
 public:
   template<typename ITER> static json to_json(ITER begin, ITER end) {
-    json j = json::object_type();
-    auto& obj = j.get<json::object_type>();
-    for(ITER it = begin; it != end; it++){
-      obj.insert(*it);
-    }
-    return j;
+    return create([&](json::object_type& obj){
+      obj.insert(begin, end);
+    });
   }
 
   template<typename T> static json to_json(const T& src){
     return to_json(std::cbegin(src), std::cend(src));
   }
 
-  static json create(std::function<void(json::object_type&)> f = std::function<void(json::object_type&)>()) {
-    json j = json::object_type();
-    auto& obj = j.get<json::object_type>();
-    if(f){
-      f(obj);
-    }
+  static json create(std::function<void(json::object_type&)> f) {
+    json j = create();
+    f(j.get<json::object_type>());
     return j;
+  }
+
+  static json create() {
+    return json::object_type();
   }
 
   static json& edit(json& j, std::function<void(json::object_type&)> f){
